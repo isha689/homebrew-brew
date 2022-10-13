bf515d5ddd60d5b538d9a31aba0b3ecd907e18b7
Isha Bansal <55244783+isha689@users.noreply.github.com>
2022-10-13 19:53:41 +0530

    Update twiliodraft.rb

--- Formula/twiliodraft.rb
+++ Formula/twiliodraft.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Twiliodraft < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://github.com/isha689/twilio-cli/releases/latest/download//twilio-3.1.0-draft.3.tar.gz"
+  url "https://github.com/isha689/twilio-cli/releases/latest/download/twilio-3.1.0-draft.3.tar.gz"
   version "5.1.8-draft.1"
   sha256 "ad71f4ce3012b6264c1a9dc7858d0ce42e5231931e5712aedec08d34cd6f5cdd"
   depends_on "node"
