114fd89bbb77f7986fa9366811ba70c11372de98
Isha Bansal <55244783+isha689@users.noreply.github.com>
2022-10-13 20:48:53 +0530

    Update update-formula.sh

--- .github/scripts/update-formula.sh
+++ .github/scripts/update-formula.sh
@@ -14,7 +14,7 @@ if [ "$version" != "$current_version" ]; then
   sed -i.bak "s/^class [^ ]*/class $class_name/" Formula/$formula@$current_version.rb
 fi
 formula_path="Formula/$formula.rb"
-sed -E -i.bak "s/twilio-v$version_pattern/twilio-v$version/g" "$formula_path"
+sed -E -i.bak "s/twilio-$version_pattern/twilio-$version/g" "$formula_path"
 sed -i.bak "s/version .*/version \"$version\"/" "$formula_path"
 sed -i.bak "s/sha256 .*/sha256 \"$sha\"/" "$formula_path"
 echo "Git configurations"
