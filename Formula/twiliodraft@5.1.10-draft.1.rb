cd220e582fdcd7cc81e84f2229b6c83fa67ef556
twilio-dx <team_interfaces+github@twilio.com>
2022-10-17 18:23:17 +0000

    Release 5.1.10-draft.2

--- Formula/twiliodraft@5.1.10-draft.1.rb
+++ Formula/twiliodraft@5.1.10-draft.1.rb
@@ -1,11 +1,11 @@
 require "language/node"
 
-class Twiliodraft < Formula
+class TwiliodraftAT5110Draft1 < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://github.com/isha689/twilio-cli/releases/download/5.1.10-draft.2/twilio-5.1.10-draft.2.tar.gz"
-  version "5.1.10-draft.2"
-  sha256 "b3e5476bfee16ee736a1487f5766c36bede597907418d73b4535bba168458d58"
+  url "https://github.com/isha689/twilio-cli/releases/download/5.1.10-draft.1/twilio-5.1.10-draft.1.tar.gz"
+  version "5.1.10-draft.1"
+  sha256 "312344eb8c2e921d6d6a5224daf500e1d15dbe3d100d6188154398008a51f98a"
   depends_on "node"
 
   def install
